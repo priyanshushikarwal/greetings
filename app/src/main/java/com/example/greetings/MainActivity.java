e99a5df910cc244c6d171498426305326ecb933f
DESKTOP <priyanshushikarwal@gmail.com>
2023-09-07 23:35:38 +0530

    first app

---
+++ app/src/main/java/com/example/greetings/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.greetings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    ImageView imageView;
+    Button mybtn;
+
+    @SuppressLint({"MissingInflatedId", "WrongViewCast"})
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        imageView = findViewById(R.id.logo);
+        mybtn = findViewById(R.id.btn);
+
+        mybtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputName = editText.getText().toString();
+                Toast.makeText(MainActivity.this, "Welcome " +inputName+"our App", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
